e6b9ef433d0ac4a2269d26b5dcb9c10ab22009bb
TalesSilva <tales.silva@gafit.com.br>
2022-08-14 05:58:08 -0300

    inserindo outros models

---
+++ src/model/Compra.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Compra {
+	private int clienteId;
+	private int codigoCompra;
+	private Double valorTotal;
+//	private Date dataCompra;
+	
+	
+	public int getClienteId() {
+		return clienteId;
+	}
+	public void setClienteId(int clienteId) {
+		this.clienteId = clienteId;
+	}
+	public int getCodigoCompra() {
+		return codigoCompra;
+	}
+	public void setCodigoCompra(int codigoCompra) {
+		this.codigoCompra = codigoCompra;
+	}
+	public Double getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(Double valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	
+}
